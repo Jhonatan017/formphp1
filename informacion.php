aa0455cfdd69a579e1f32fcff0d1a5cd5be64196
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-13 16:17:03 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion = $_POST['Identificacion'];
+$NomApellidos = $_POST['NomApellidos'];
+$Cargo= $_POST['Cargo'];
+$Salario= $_POST['Salario'];
+echo "LA IDENTIFICACION DEL EMPLEADO ES: ".$Identificacion;
+echo "LOS NOMBRES DEL EMPLEADO SON: ".$NomApellidos;
+echo "EL CARGO DEL EMPLEADO ES: ".$Cargo;
+echo "EL SALARIO DEL EMPLEADO ES: ".$Salario;
+?>
