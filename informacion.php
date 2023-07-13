351e069c3c0d609419b7dccce9e84867a7a9b21a
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-13 17:02:19 -0500

    Update and rename informacion.php to informacion.html

--- informacion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Camisa = $_POST['Camisa'];
-$Pantalon = $_POST['Pantalon'];
-$Precio1= $_POST['Precio1'];
-$Precio2= $_POST['Precio2'];
-$Suma= $_POST['Precio1+Precio2'];
-echo "El total a pagar es: ".$Suma;
-?>
