99b42b0cd0af2cc6be547180d828ce37fbf66414
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-18 16:24:22 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,11 +1,11 @@
-$Producto1=$_POST ['Producto1'];
-$Producto2=$_POST ['Producto2'];
+$Producto1=$_POST ['Product1'];
+$Producto2=$_POST ['Product2'];
 $Precio=$_POST ['Precio1'];
 $Precio=$_POST ['Precio2'];
 $suma=Precio1+Precio2
-echo "el nombre de producto es: ".$Precio1;
+echo "el nombre de producto es: ".$product1;
 echo "el precio del producto es: ".$Precio2;
-echo "el nombre de producto es: ".$Producto1;
+echo "el nombre de producto es: ".$Product2;
 echo "el precio del producto es:  ".$Producto2;
 echo "el total a pagar es:  ".$suma;
 ?>
